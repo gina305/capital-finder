27aa35c0c34c8ecac6c5ac106e33406ee965619d
gina305 <napier.gina@gmail.com>
2022-08-09 20:51:05 -0400

    Added missing file

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -8,23 +8,24 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    
+    msg = ""
     url_components = parse.urlsplit(self.path)
     query_string_list = parse.parse_qsl(url_components.query)
     
   
-
+    #Store user request values
     query_dict = dict(query_string_list)
-    msg = str(query_dict)
-
-
-
     
 
+    #Extract only the country
     for key, value in query_dict.items():
       if str(key.lower()) == "country":
         msg = str(value)
 
-   
+    if msg == "":
+      self.wfile.write("Invalid query. Enter a country. I.e. /api/capital-finder?country=Bahamas".encode())
+    else:
+      self.wfile.write(msg.encode())
+
     self.wfile.write(msg.encode())
     return
