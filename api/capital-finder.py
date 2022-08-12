73f976c89e18fc94484777e332e44a91f0fdc922
gina305 <napier.gina@gmail.com>
2022-08-12 08:51:22 -0400

    updated code:

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,31 +1,43 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
 
-    #Store user request values
-    query_dict = dict(query_string_list)
 
-    #Extract only the country
-    for key, value in query_dict.items():
-      if str(key.lower()) == "country":
-        msg = str(value)
-        url = 'https://restcountries.com/v3.1/name/' + msg
+class handler(BaseHTTPRequestHandler):
 
-          #Create a http request
+    def do_GET(self):
+        # Necessary variables for parsing through string; also contains dicitonary
+        url_components = parse.urlsplit(self.path)
+        query_string_list = parse.parse_qsl(url_components.query)
+        query_dict = dict(query_string_list)
 
-        break
-    r = requests.get(url)
+        if 'name' in query_dict:
+            # if the country exists, follow through code below
 
-              #Saves the response as a dictionary
-    r_objects = r.json() 
+            url = 'https://restcountries.com/v3.1/name/'
+            query = query_dict['name']
+            query_url = url + query
 
-              #Extract and save the capital of the response
-    capital = r_objects[0]
-    capital = capital.get('capital')[0]
-  
-    user_response = f"The capital of {msg} is {capital}."
+            response = requests.get(query_url)
+            data = response.json()
 
-    if user_response == "":
-      self.wfile.write("Please enter a valid request (i.e. /api/capital-finder?country=Bahamas)".encode())  
-    
-    else:
-      self.wfile.write(user_response.encode()) 
+            parsed_city = data[0]['capital']
+            city = str(parsed_city[0])
+            result_str = f"The capital of {query.upper()} is {city.upper()}"
 
-    return
+            # This is in both statements so whatever content returned is consistent
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+
+            self.wfile.write(result_str.encode())
+        else:
+            # if the country doesn't exist/user error, return message below
+            error_message = "Please search for a valid capital city to receive a valid country."
+
+            # self.send_response(200)
+            # self.send_header('Content-type', 'text/plain')
+            # self.end_headers()
+
+            self.wfile.write(error_message.encode())
+        return
