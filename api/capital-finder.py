398c1b4d7b4df4ea65865844d8a154be4c68232b
gina305 <napier.gina@gmail.com>
2022-08-16 17:18:13 -0400

    Updated logic to capital finder

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -6,33 +6,34 @@ import requests
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        # set variables 
-        url_components = parse.urlsplit(self.path)
-        query_string_list = parse.parse_qsl(url_components.query)
+        # Necessary variables for parsing through string; also contains dicitonary
+        params = parse.urlsplit(self.path)
+        query_string_list = parse.parse_qsl(params.query)
         query_dict = dict(query_string_list)
 
         if 'name' in query_dict:
-         
-            url = 'https://restcountries.com/v3.1/name/'
-            query = query_dict['name']
-            query_url = url + query
-
-            response = requests.get(query_url)
-            data = response.json()
-
-            parsed_city = data[0]['capital']
-            city = str(parsed_city[0])
-            result_str = f"The capital of {query.upper()} is {city.upper()}"
-
-            # This is in both statements so whatever content returned is consistent
+              # set server settings
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
+            
+            # if the country exists, follow through code below
+            query = query_dict['name']
 
-            self.wfile.write(result_str.encode())
+            #Request API data for this query
+            query_url = 'https://restcountries.com/v3.1/name/' + query
+            response = requests.get(query_url)
+            data = response.json()
+
+            #Extract query from returned data
+            city = data[0]['capital']
+            city = str(city[0])
+            result = f"The capital of {query} is {city}."
+            
+            self.wfile.write(result.encode())
         else:
-            # if the country doesn't exist/user error, return message below
-            error_message = "Please search for a valid capital city to receive a valid country."
 
-            self.wfile.write(error_message.encode())
+            self.wfile.write("Invalid entry. Try something like .../api/capital-finder/name=Bahamas".encode())
         return
+
+# non-country is getting pushed into query_dict and therefore, gets passed into the "it statement". So it ends in a 502 and doesn't get to the else statement.
