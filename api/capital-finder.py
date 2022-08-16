d61a34c77b2c76651e167d6ec1c18a46fb88c11a
gina305 <napier.gina@gmail.com>
2022-08-16 18:11:45 -0400

    Updated capital finder and country finder

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -27,7 +27,7 @@ class handler(BaseHTTPRequestHandler):
 
             #Extract query from returned data
             city = data[0]['capital']
-            city = city[0]
+            city = str(city[0])
             result = f"The capital of {query.upper()} is {city.upper}."
             
             self.wfile.write(result.encode())
