dcc7229dd91f1292da1c5fe38d5ce6d6de686d20
Gina <5680013+gina305@users.noreply.github.com>
2022-09-09 00:15:50 -0400

    Update capital-finder.py

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
-
+import os
 
 class handler(BaseHTTPRequestHandler):
 
@@ -11,25 +11,19 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(params.query)
         query_dict = dict(query_string_list)
 
-        if 'name' in query_dict:
+        if 'url' in query_dict:
               # set server settings
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             
             # if the country exists, follow through code below
-            query = query_dict['name']
+            source = query_dict['url']
 
-            #Request API data for this query
-            query_url = 'https://restcountries.com/v3.1/name/' + query
-            response = requests.get(query_url)
-            data = response.json()
-
-            #Extract query from returned data
-            parsed_city = data[0]['capital']
-            city = str(parsed_city[0])
-            result = f"The capital of {query.upper()} is {city.upper()}."
-            self.wfile.write(result.encode())
+            x= os.system(f"mammoth {source} output.html")
+        
+            
+            self.wfile.write(x.encode())
         else:
 
             self.wfile.write("Invalid entry. Try something like .../api/capital-finder/name=Bahamas".encode())
